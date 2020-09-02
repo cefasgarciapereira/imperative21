2a8a5a3c155615ad86697c70f8ac155c7412a8f6
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-02 14:40:37 -0300

    Custom Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,5 +2,11 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export default function Button(props){
-    return <button className={props.active ? styles.active_button : styles.button}>{props.label}</button>
+    return (
+        <button 
+        className={props.active ? styles.active_button : styles.button}
+        >
+            {props.label}
+        </button>
+    )
 }
