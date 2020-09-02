80487c9a5cc1bf8ee7d37635839842b8fd021e21
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-02 14:41:59 -0300

    Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,9 @@ import Hero from '../sections/hero'
 import {default as Section1} from '../sections/hero/ContentSection'
 import TheFacts from '../sections/TheFacts'
 import {default as Section2} from '../sections/TheFacts/ContentSection'
+import {default as Section3} from '../sections/TheFacts/SimpleStat'
+import MoneyStack from '../sections/TheFacts/MoneyStack'
+import Footer from '../sections/Footer'
 import styles from './index.module.css'
 
 export default function Home() {
@@ -13,6 +16,9 @@ export default function Home() {
       <Section1/>
       <TheFacts/>
       <Section2/>
+      <Section3/>
+      <MoneyStack/>
+      <Footer/>
     </div>
   )
 }
