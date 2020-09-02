616c82a826bfcc38f427e3e0c156887c71a2f976
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-02 15:37:57 -0300

    Index com novos components

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,8 +3,9 @@ import Hero from '../sections/hero'
 import {default as Section1} from '../sections/hero/ContentSection'
 import TheFacts from '../sections/TheFacts'
 import {default as Section2} from '../sections/TheFacts/ContentSection'
-import {default as Section3} from '../sections/TheFacts/SimpleStat'
+import SimpleStat from '../sections/TheFacts/SimpleStat'
 import MoneyStack from '../sections/TheFacts/MoneyStack'
+import BannerStat from '../sections/TheFacts/BannerStat'
 import Footer from '../sections/Footer'
 import styles from './index.module.css'
 
@@ -16,8 +17,21 @@ export default function Home() {
       <Section1/>
       <TheFacts/>
       <Section2/>
-      <Section3/>
+      <SimpleStat
+      prefix="26 individuals own"
+      firstLine="more wealth than"
+      secondLine="half of humanity"
+      credit="Oxfam 2019"
+      />
       <MoneyStack/>
+      <SimpleStat
+      prefix="The median family wealth
+      for white people in the US is"
+      firstLine="10x higher than it"
+      secondLine="is for Black people"
+      credit="United States Federal Reserve System, 2016"
+      />
+      <BannerStat/>
       <Footer/>
     </div>
   )
