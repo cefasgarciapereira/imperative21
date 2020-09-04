e89a5a8a240dff529864f312478f4d51ed1d210e
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-03 22:12:08 -0300

    Adiciona animacao ao Callout, MoneyStats e StatiscContent da secao The Facts

--- src/sections/TheFacts/Callout/index.js
+++ src/sections/TheFacts/Callout/index.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import styles from './styles.module.css'
+import Fade from 'react-reveal/Fade';
 
 export default function Callout(){
     return(
         <div className={styles.callout}>
-            <div className={styles.callout_inner}>
-                <h2>Stakeholder Capitalism is Working</h2>
-                <p>The top 20% of companies ranked by JUST Capital for stakeholder leadership 
-                    have outperformed the bottom 20% by 4.7% through the current bear market
-                     and the first signs of recovery.</p>
-                <p className={styles.credit}>JUST Capital</p>
-            </div>
+            <Fade cascade clear>
+                <div className={styles.callout_inner}>
+                    <h2>Stakeholder Capitalism is Working</h2>
+                    <p>The top 20% of companies ranked by JUST Capital for stakeholder leadership 
+                        have outperformed the bottom 20% by 4.7% through the current bear market
+                        and the first signs of recovery.</p>
+                    <p className={styles.credit}>JUST Capital</p>
+                </div>
+            </Fade>
         </div>
     )
 }
--- src/sections/TheFacts/StatisticPeople/index.js
+++ src/sections/TheFacts/StatisticPeople/index.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import styles from './styles.module.css'
 import people from '../../../../public/people.svg'
+import Fade from 'react-reveal/Fade'
 
 export default function StatisticPeople(){
     return(
-        <div className={styles.statistic_people}>
-            <div className={styles.statistic_inner}>
-                <div className={styles.statistic_group}>
-                    <p className={styles.statistic_support}>
-                        "COVID really lays out the limits of thinking of people as solely 
-                        interchangeable units and not as human beings, treating them with 
-                        dignity and respect as an integral part of the production enterprise"
-                    </p>
-                    <img src={people}></img>
-                    <p className={styles.statistic_credit}>
-                        Rebecca Henderson, Harvard Business School professor
-                        <br/>
-                        and author of “Reimagining Capitalism in a World on Fire”
-                    </p>
+            <div className={styles.statistic_people}>
+                <div className={styles.statistic_inner}>
+                    <Fade cascade bottom>
+                        <div className={styles.statistic_group}>
+                            <p className={styles.statistic_support}>
+                                "COVID really lays out the limits of thinking of people as solely 
+                                interchangeable units and not as human beings, treating them with 
+                                dignity and respect as an integral part of the production enterprise"
+                            </p>
+                            <img src={people}></img>
+                            <p className={styles.statistic_credit}>
+                                Rebecca Henderson, Harvard Business School professor
+                                <br/>
+                                and author of “Reimagining Capitalism in a World on Fire”
+                            </p>
+                        </div>
+                </Fade>
                 </div>
             </div>
-        </div>
     )
 }
