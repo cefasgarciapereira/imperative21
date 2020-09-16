5254d829c33785315252125b2a1db12fea319e51
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-16 17:36:08 -0300

    Troca do conteudo da Seçao de Imperativos

--- src/sections/TheVision/ContentSection2/index.js
+++ src/sections/TheVision/ContentSection2/index.js
@@ -23,13 +23,44 @@ export default function ContentSection2(){
                             em torno de diferentes trabalhos, e que aliados se aproximem e se afastem com base em suas capacidades 
                             e interesses.</p>
 
+                        
+                        <p>
+                            O objetivo de qualquer campanha é impulsionar a ação. A Redefina o Capitalismo irá conectar líderes empresariais
+                            inspirados ou interessados que ainda perguntam: “O que eu faço agora?”. Para isso, aqui estão as melhores
+                            ferramentas e recursos, oferecidos por cada uma das organizações da rede Imperative 21, com o objetivo
+                            de apoiar a criação de ações significativas.
+                        </p>
+
+                        <h3>Crie com interdependência</h3>
+
+                        <p>
+                            Um sistema econômico mais saudável que reconhece a interdependência entre pessoas, o planeta e as economias; 
+                            equilibra as relações entre o setor privado, o governo e a sociedade civil; garante que todos tenham acesso 
+                            a oportunidades seguras e justas no mercado.
+                        </p>
+
+                        <h3>Invista em mudanças estruturais</h3>
+
+                        <p>
+                            Um sistema econômico que elimina a desigualdade estrutural; garante que os cargos de liderança e as empresas 
+                            sejam mais inclusivas e que o investimento em negócios de impacto seja mais acessível; usa a tecnologia para 
+                            promover ideais democráticos e a favor dos direitos humanos; e amplifica a voz, empodera e cria oportunidade 
+                            para aqueles atualmente marginalizados.
+                        </p>
+
                         <ul>
-                            <li>Tal coalizão está aproveitando o poder da cultura para exigir um sistema econômico 
-                                que ofereça bem-estar compartilhado em um planeta saudável.</li>
-                            <li>Outra está transformando as finanças para gerenciar o risco sistêmico.</li>
-                            <li>Uma terceira está administrando um processo global para co-criar um conjunto de 
-                                "imperativos para a mudança do sistema econômico” que dão à nossa comunidade de aliados maior coerência e poder.</li>
+                            <li>Torne-se um líder responsável por justiça, equidade, diversidade e inclusão ao lado da comunidade de Empresas B: 
+                                Comprometa-se a ser uma empresa antirracista</li>
+                            <li>Leia o guia do B Lab de ação empresarial por justiça, equidade, diversidade e inclusão durante a COVID-19</li>
                         </ul>
+
+                        <h3>Considere todas as partes interessadas</h3>
+
+                        <p>
+                            Um sistema econômico que mede o sucesso com base em métricas comuns e credíveis de criação de valor sustentável 
+                            para todos os stakeholders; criar incentivos que recompensem negócios e investimentos criando valor social e ambiental; 
+                            e aprimorar os padrões do dever fiduciário.
+                        </p>
                     </div>
                 </Fade>
             </div>
