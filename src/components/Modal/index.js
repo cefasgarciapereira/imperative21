044f18cef348762f37bf85dd9b7f1819d4effe00
Cefas Garcia Pereira <cefasgarciapereira@gmail.com>
2020-09-22 19:32:21 -0300

    Altera o formulario de email para mailto

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -30,14 +30,17 @@ export default function Modal(props){
                     <h2>{props.call}</h2>
                     <p>{props.message}</p>
                 <form
-                action="https://formspree.io/moqkgngk"
-                method='POST'>
+                action="mailto:brasil@sistemab.org"
+                target="_top"
+                method='post'
+                enctype="text/plain">
                     <input className={styles.form_control} style={{gridArea: 'a'}} type="text" name="first-name" placeholder="Nome *" required/>
                     <input className={styles.form_control} style={{gridArea: 'b'}} type="text" name="last-name" placeholder="Sobrenome *" required/>
                     <input className={styles.form_control} style={{gridArea: 'c'}} type="text" name="email" placeholder="E-mail" required/>
                     <input className={styles.form_control} style={{gridArea: 'd'}} type="text" name="company" placeholder="Empresa *" required/>
                     <input className={styles.form_control} style={{gridArea: 'e'}} type="text" name="title" placeholder="Título *" required/>
                     <textarea type="text" name="message" style={{gridArea: 'f'}} placeholder="Mensagem *" required></textarea>
+                    <span style={{gridArea: 'g'}}>ou envie para  <a href="mailto:brasil@sistemab.org" target="_blank" style={{textDecoration: 'underline'}}>brasil@sistemab.org</a></span>
                     <input className={styles.button} type="submit" value="Enviar"/>
                 </form>
             </ReactModal>
