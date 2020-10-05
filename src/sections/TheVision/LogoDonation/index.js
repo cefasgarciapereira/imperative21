835eb2e738e939d621954d7e97187553d51617a7
allanroosch <allan.roosch@gmail.com>
2020-10-05 07:41:01 -0300

    novos logos sem sucesso

--- src/sections/TheVision/LogoDonation/index.js
+++ src/sections/TheVision/LogoDonation/index.js
@@ -5,6 +5,7 @@ const logo_all_set = require('../../../../static/coalisao_brasil/all_set.png')
 const logo_avante = require('../../../../static/coalisao_brasil/avante.jpeg')
 const logo_az_quest = require('../../../../static/coalisao_brasil/az_quest.jpg')
 const logo_dengo = require('../../../../static/coalisao_brasil/dengo.png')
+const logo_clearsale = require('../../../../static/coalisao_brasil/clearsale.png')
 const logo_fama = require('../../../../static/coalisao_brasil/fama.jpg')
 const logo_grupo_gaia = require('../../../../static/coalisao_brasil/grupo_gaia.png')
 const logo_reserva = require('../../../../static/coalisao_brasil/reserva.png')
@@ -25,6 +26,7 @@ export default function LogoDonation(){
                             <li className={styles.logo_grid_item}><a href="https://www.avante.com.vc/"><img src={logo_avante} alt="Logo Avante"/></a></li>
                             <li className={styles.logo_grid_item}><a href="http://azquest.com.br/"><img src={logo_az_quest} alt="Logo AZ Quest"/></a></li>
                             <li className={styles.logo_grid_item}><a href="https://www.bresco.com.br/"><img src={logo_bresco} style={{marginLeft: '3rem', width: '80%'}} alt="Logo Bresco"/></a></li>
+                            <li className={styles.logo_grid_item}><a href="https://br.clear.sale/"><img src={logo_clearsale} alt="Logo Clearsale"/></a></li>
                             <li className={styles.logo_grid_item}><a href="https://dengo.com/"><img src={logo_dengo} style={{width: '60%'}} alt="Logo Dengo"/></a></li>
                             <li className={styles.logo_grid_item}><a href="https://www.famainvestimentos.com.br/"><img src={logo_fama} alt="Logo Fama"/></a></li>
                             <li className={styles.logo_grid_item}><a href="https://grupogaia.com.br/"><img src={logo_grupo_gaia} style={{width: '70%'}} alt="Logo Grupo Gaia"/></a></li>
